c074d7e46498788da9a00021e76b8a47226147f4
Radek <radoslaw.parol95@gmail.com>
2023-01-16 23:16:37 +0100

    add cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000/costco',
+    specPattern: 'cypress/e2e/**/*.{js, jsx}',
+    excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*']
   },
 });
